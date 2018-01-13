7eed87ac1cad259157fc35a794defec2b40602cb
Nicholas Guest <nicholasguest@MacBook-Pro.local>
2018-01-12 17:47:34 -0800

    update to THREE 0.89.0

--- src/app/helpers/material.js
+++ src/app/helpers/material.js
@@ -12,7 +12,7 @@ export default class Material {
 
     this.standard = new THREE.MeshStandardMaterial({
       color,
-      shading: THREE.FlatShading,
+      flatShading: true,
       roughness: 1,
       metalness: 0,
       side: THREE.DoubleSide
